c892ccbae73edc344021548a76d7414a7e147d3f
Fides Escalona <fescalonamolina@gmail.com>
2020-05-01 08:30:29 -0500

    Primer commit curso avanzado de React Platzi hasta hooks y react apollo y graphql

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+import { App } from './App'
+
+const client = new ApolloClient({
+  uri: 'https://petgram.fgescalona.now.sh/graphql'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app'))
