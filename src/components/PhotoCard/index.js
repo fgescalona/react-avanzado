2edc615fe674177245cf4b1f233903a40bd724e5
Fides Escalona <fescalonamolina@gmail.com>
2020-05-01 19:31:02 -0500

    Se agrega Reach Router para manejo de rutas de la SPA

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,10 +1,13 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react'
+import React, { Fragment } from 'react'
 import { Article, ImgWrapper, Img } from './styles'
+
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
+
+import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -17,22 +20,24 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     <Article ref={element}>
       {
         show && <Fragment>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
-          
+          </Link>
+
           <ToggleLikeMutation>
             {
               (toggleLike) => {
                 const handleFavClick = () => {
                   !liked && toggleLike({ variables: {
                     input: { id }
-                  }})
+                  } })
+
                   setLiked(!liked)
                 }
-                return <FavButton liked={liked} likes={likes} onClick={handleFavClick}/>
+
+                return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
               }
             }
           </ToggleLikeMutation>
